ed0e9257d8d6d4a5d89ea31cf5e6c994d09d1c31
sanika <sanikaraul06@gmail.com>
2020-11-05 17:22:33 +0530

    make it better

--- index.js
+++ index.js
@@ -28,13 +28,128 @@ const dialogflowFulfillment = (request, response) => {
       result=result+1;
       console.log(result);
       agent.add("Okay Cool we are done with the first question your current score is noted!")
+      agent.add("Okay so let us move on to the second question !!You are blindfolded and 10 coins are place in front of you on table. You are allowed to touch the coins, but can’t tell which way up they are by feel. You are told that there are 5 coins head up, and 5 coins tails up but not which ones are which.Can you make two piles of coins each with the same number of heads up? You can flip the coins any number of times. Please answer in Yes or No")
+    
 	 }
+     else
+     {
      agent.add("Okay Cool we are done with the first question your current is noted!")
-     agent.add("So the second question is blah blah")
+     agent.add("Okay so let us move on to the second question !!You are blindfolded and 10 coins are place in front of you on table. You are allowed to touch the coins, but can’t tell which way up they are by feel. You are told that there are 5 coins head up, and 5 coins tails up but not which ones are which.Can you make two piles of coins each with the same number of heads up? You can flip the coins any number of times. Please answer in Yes or No")
+     }
 	}
+
+
+    function apti2(agent)
+    {
+     const apt2=agent.parameters.apti2;
+     if (apt2=='yes')
+     {
+      result=result+1;
+      console.log(result);
+      agent.add("Okay Cool we are done with the second question your current score is noted!")
+      agent.add("Okay so let us move on to the third question !!There are 3 ants sitting on three corners of a triangle. All ants randomly pick a direction and start moving along edge of the triangle. What is the probability that any two ants collide?Hint: Every ant has two choices (pick either of two edges going through the corner on which ant is initially sitting)")
+      agent.add("Give me the answer in fraction")
+	 }
+     else
+     {
+     agent.add("Okay Cool we are done with the second question your current score is noted!")
+     agent.add("Okay so let us move on to the third question !!There are 3 ants sitting on three corners of a triangle. All ants randomly pick a direction and start moving along edge of the triangle. What is the probability that any two ants collide?Hint: Every ant has two choices (pick either of two edges going through the corner on which ant is initially sitting)")
+     agent.add("Give me the answer in fraction")
+     }
+	}
+
+
+       function apti3(agent)
+    {
+     const apt3=agent.parameters.apti3;
+     if (apt3==6/8)
+     {
+      result=result+1;
+      console.log(result);
+      agent.add("Okay Cool we are done with the third question your current score is noted!")
+      agent.add("Okay so let us move on to the forth question !! In the following question, one term in the number series is wrong. Find the wrong term.125, 126, 124, 127, 122, 128")
+
+	 }
+     else
+     {
+     agent.add("Okay Cool we are done with the third question your current score is noted!")
+     agent.add("Okay so let us move on to the forth question !! In the following question, one term in the number series is wrong. Find the wrong term.125, 126, 124, 127, 122, 128")
+
+     }
+	}
+
+        function apti4(agent)
+    {
+     const apt4=agent.parameters.apti4;
+     if (apt4==122)
+     {
+      result=result+1;
+      console.log(result);
+      agent.add("Okay Cool we are done with the forth question your current score is noted!")
+      agent.add("Okay so let us move on to the fifth question !! ")
+      agent.add(" All Lamels are Signots with buttons.No yellow Signots have buttons.No Lamels are yellow.If the first two statements are true, the third statement is True or False?")
+	 }
+     else
+     {
+      agent.add("Okay Cool we are done with the forth question your current score is noted!")
+      agent.add("Okay so let us move on to the fifth question !! ")
+      agent.add(" All Lamels are Signots with buttons.No yellow Signots have buttons.No Lamels are yellow.If the first two statements are true, the third statement is True or False?")
+	
+     }
+	}
+
+      function apti5(agent)
+    {
+     const apt5=agent.parameters.apti5;
+     if (apt5=='True' || apt5=='true')
+     {
+      result=result+1;
+      console.log(result);
+      agent.add("Okay Cool we are done with the fifth question your current score is noted!")
+      agent.add("Okay so let us move on to the sixth question !! ")
+      agent.add("What was the day on 15th august 1947 ? ")
+	 }
+     else
+     {
+      agent.add("Okay Cool we are done with the fifth question your current score is noted!")
+      agent.add("Okay so let us move on to the sixth question !! ")
+      agent.add("What was the day on 15th august 1947?")
+     }
+	}
+
+
+     function apti6(agent)
+    {
+     const apt6=agent.parameters.apti6;
+     if (apt6=='Friday' || apt5=='friday')
+     {
+      result=result+1;
+      console.log(result);
+      agent.add("Okay Cool we are done with the sixth question your current score is noted!")
+      agent.add(" And your final result is"+result)
+	 }
+     else
+     {
+      agent.add("Okay Cool we are done with the sixth question your current score is noted!")
+      agent.add(" And your final result is"+result)
+     }
+	}
+
+
+
+
+
+
+
+
     let intentMap = new Map();
     intentMap.set("Default Welcome Intent", sayHello)
     intentMap.set("Aptitude1", apti1)
+    intentMap.set("Aptitude2", apti2)
+    intentMap.set("Aptitude3", apti3)
+    intentMap.set("Aptitude4", apti4)
+    intentMap.set("Aptitude5", apti5)
+    intentMap.set("Aptitude6", apti6)
     agent.handleRequest(intentMap)
 
 }
