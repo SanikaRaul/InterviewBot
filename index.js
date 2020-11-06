53731e7aa0670c1c5282396ad8ef825ba6c1234b
sanika <sanikaraul06@gmail.com>
2020-11-06 17:23:51 +0530

    make it better

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const dialogflowFulfillment = (request, response) => {
          agent.add("Okay Cool we are done with the first question your current score is noted!")
 
          agent.add("Okay so let us move on to the second question !!You are blindfolded and 10 coins are place in front of you on table. You are allowed to touch the coins, but can’t tell which way up they are by feel. You are told that there are 5 coins head up, and 5 coins tails up but not which ones are which.Can you make two piles of coins each with the same number of heads up? You can flip the coins any number of times. Please answer in Yes or No")
-         agent.add("Options for this question are (A) 67 (B) 40 (C) 45 (D) 55")
+        
 	}
 
 
