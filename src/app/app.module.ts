7461d054993c198d24f2094d4b257eed9ac5bb9b
Shraddha Bijotkar <shraddha.bijotkar@gmail.com>
2021-03-02 13:53:11 +0530

    Front end updated- shraddha

---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { DetailsComponent } from './details/details.component';
+
+
+//angular material
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatListModule} from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    FormsModule,  
+    MatListModule,
+    BrowserAnimationsModule
+
+
+
+
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/details/details.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl , FormGroup, Validators, FormBuilder} from '@angular/forms';
+
+
+interface Lang {
+  value: string;
+  viewValue: string;
+}
+interface Db {
+  value: string;
+  viewValue: string;
+}
+interface Level {
+  value: string;
+  viewValue: string;
+}
+interface Shell {
+  value: string;
+  viewValue: string;
+}
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  selectedLang: string;
+  languages: Lang[] = [
+    {value: 'python', viewValue: 'Python'},
+    {value: 'java', viewValue: 'Java'},
+    {value: 'cpp', viewValue: 'Cpp'}
+  ];
+  
+  databases: Db[] = [
+    {value:'sql',viewValue:'SQL'},
+    {value:'Nosql',viewValue:'NoSQL'},
+  ]
+  levels: Level[]= [
+    {value:'beginner', viewValue:"Beginner"},
+    {value: 'intermediate', viewValue:'Intermediate'},
+    {value: 'expert', viewValue:'Expert'}
+  ]
+  shells: Shell[] = [
+    {value:'yes', viewValue:'yes'},
+    {value:'no', viewValue:'no'}
+  ]
+  constructor (private fb: FormBuilder){}
+
+    studentData = this.fb.group({
+
+    firstName : ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+    lastName : ['', [Validators.required,Validators.pattern('^[a-zA-Z]+$')]],
+    lang: ['',Validators.required],
+    db : ['',Validators.required],
+    shell: ['',Validators.required],
+    dslevel: ['',Validators.required]
+
+
+    })
+
+    
+  
+
+  ngOnInit() {
+
+  }
+  onSubmit(studentData){
+    console.log(this.studentData.value);
+    console.log(this.firstName);
+    this.studentData.reset();
+    this.studentData.markAsUntouched();
+    this.studentData.markAsPristine();
+  }
+  
+  get firstName(){
+   return this.studentData.controls['firstName'];
+  }
+  get lastName(){
+    return this.studentData.controls['lastName'];
+  }
+  get language(){
+    return this.studentData.controls['lang'];
+  }
+  get db(){
+    return this.studentData.controls['db'];
+  }
+  get shell(){
+    return this.studentData.controls['shell'];
+
+  }
+  get ds(){
+    return this.studentData.controls['dslevel'];
+
+  }
+}
